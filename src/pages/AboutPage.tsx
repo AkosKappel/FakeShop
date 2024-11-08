5d86f861e055a9bc3fa4a17f6105826a22bbc6e1
Akos Kappel <akos.kappel@svarba.sk>
2024-11-08 11:00:40 +0100

    update about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,42 +1,49 @@
+import reactIcon from '../assets/react.svg';
+import viteIcon from '../assets/vite.svg';
+import tailwindIcon from '../assets/tailwind.svg';
+
 const AboutPage = () => {
   return (
     <div className="m-8 flex items-center justify-center flex-col gap-3">
       <h1 className="text-4xl font-bold text-gray-800">About FakeShop</h1>
-      <p className="text-lg text-gray-700">
-        <i className="text-pink-500 font-semibold">FakeShop</i> is a mock online
-        store built with
+      <p className="text-lg text-gray-700 text-center">
+        <a href="/">
+          <i className="text-pink-500 font-semibold">FakeShop</i> is a mock
+          online store built with{' '}
+        </a>
         <a
           href="https://reactjs.org"
           target="_blank"
           rel="noreferrer"
-          className="text-pink-500 font-semibold"
+          className="text-pink-500 font-semibold inline-flex items-center gap-1"
         >
-          {' '}
-          React
+          React <img src={reactIcon} alt="React logo" className="w-6 h-6" />
         </a>
         ,{' '}
         <a
           href="https://vitejs.dev"
           target="_blank"
           rel="noreferrer"
-          className="text-pink-500 font-semibold"
+          className="text-pink-500 font-semibold inline-flex items-center gap-1"
         >
           Vite
+          <img src={viteIcon} alt="Vite logo" className="w-6 h-6" />
         </a>
         , and{' '}
         <a
           href="https://tailwindcss.com"
           target="_blank"
           rel="noreferrer"
-          className="text-pink-500 font-semibold"
+          className="text-pink-500 font-semibold inline-flex items-center gap-1"
         >
           Tailwind CSS
+          <img src={tailwindIcon} alt="Tailwind CSS logo" className="w-6 h-6" />
         </a>
         .
       </p>
-      <p className="text-lg text-gray-700">
-        It is a simple online store for shopping. The products are fetched from
-        a free{' '}
+      <p className="text-lg text-gray-700 text-center">
+        It is a simple frontend application showcasing online store with
+        products that are fetched from a free and public{' '}
         <a
           href="https://fakestoreapi.com"
           target="_blank"
@@ -47,7 +54,7 @@ const AboutPage = () => {
         </a>
         .
       </p>
-      <p className="text-lg text-gray-700">
+      <p className="text-lg text-gray-700 text-center">
         Check out the source code on{' '}
         <a
           href="https://github.com/AkosKappel/FakeShop"
